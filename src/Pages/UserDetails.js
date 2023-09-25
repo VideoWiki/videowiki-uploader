b596bbd0460b0d05716b10b85d7b9df1436b6a4f
Ritik2712 <ritik27122mahajan@gmail.com>
2023-09-25 10:22:19 +0530

    fix

--- src/Pages/UserDetails.js
+++ src/Pages/UserDetails.js
@@ -60,7 +60,7 @@ const UserDetails = () => {
       setWalletAddress(storedUser.walletAddress);
       setMemonic(storedUser.memonic);
       setTodos(storedUser.todos);
-      // getTodos(storedUser.userName);
+      getTodos(storedUser.userName);
     } else {
       localStorage.setItem(
         "user",
